1cb5bcace920581f817c19104a265b1018afe770
syedmuneebali99 <68074148+syedmuneebali99@users.noreply.github.com>
2021-10-01 03:59:07 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = process.env.PORT || 7000;
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res, next) => {
