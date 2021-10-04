9ed80325aa8e071f89e910590096fec174fd5fc6
imrcht <rachchooit@gamil.com>
2021-10-04 19:22:55 +0530

    Removed

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const control = require("../controllers/auth");
-
-router.post("/register", control.Postregister);
-router.post("/login", control.Postlogin);
-
-module.exports = router;
