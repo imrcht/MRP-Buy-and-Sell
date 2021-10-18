2da26b9fea5bb0b8f8b833150ce50a177970905d
imrcht <rachchooit@gamil.com>
2021-10-18 17:55:58 +0530

    images name change to img

--- server.js
+++ server.js
@@ -16,10 +16,6 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
-app.get('/',(req, res, next)=>{
-	res.render('Home_page.ejs')
-})
-
 const server = app.listen(PORT, () => {
 	console.log(
 		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`
