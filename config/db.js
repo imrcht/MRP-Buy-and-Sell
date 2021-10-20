84bf84926d1c6960c43267aa02116f0c27c0f260
imrcht <rachchooit@gamil.com>
2021-10-20 21:00:58 +0530

    auth changes

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const confi = require('../secret');
 
-const MONGO_URI = `mongodb+srv://MRP:mrpsaleproject@cluster0.7ohbj.mongodb.net/MrpSaleProject`;
+const MONGO_URI = `mongodb+srv://${confi.user}:${confi.pwd}@cluster0.7ohbj.mongodb.net/MrpSaleProject`;
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(MONGO_URI, {
