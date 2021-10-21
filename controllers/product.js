63363ddf8017c1f4c911bfa30274906bc1059871
imrcht <rachchooit@gamil.com>
2021-10-21 20:41:40 +0530

    changes in product controller

--- controllers/product.js
+++ controllers/product.js
@@ -6,51 +6,87 @@ const asyncHandler = require("../middleware/async");
 const deleteFile = require("../utils/removeImage");
 
 exports.getProductForm = asyncHandler(async (req, res, next) => {
-  // here in this we will render the product adding form page
-  res.render("product/productForm");
+	// here in this we will render the product adding form page
+	res.render("product/productForm");
 });
 
 exports.getAllProducts = asyncHandler(async (req, res, next) => {
-  const products = await Product.find();
+	const products = await Product.find();
+	res.status(200).render("product", {
+		products: products,
+	});
 });
 
 exports.getProductsByCategory = asyncHandler(async (req, res, next) => {
-  const category = req.query.category;
-  const productsByCategory = await Product.find({ category: category });
+	const category = req.query.category;
+	const products = await Product.find({ category: category });
+	res.status(200).render("product", {
+		products: products,
+	});
 });
 
 exports.getProductById = asyncHandler(async (req, res, next) => {
-  const productId = req.param.productId;
-  const product = await Product.findById(productId);
+	const productId = req.param.productId;
+	const product = await Product.findById(productId);
+	if (!product) {
+		return next(
+			new errorResponse(`Resource not found of id ${productId}`, 404),
+		);
+	} else {
+		res.status(200).render("product", {
+			product: product,
+		});
+	}
 });
 
 exports.postProduct = asyncHandler(async (req, res, next) => {
-  const title = req.body.title;
-  const category = req.body.category;
-  const description = req.body.description;
-  const image = req.file;
-  const cost = req.body.cost;
+	const product = await Product.create(req.body);
+
+	res.status(201).render("product", {
+		product,
+	});
 });
 
 exports.updateProduct = asyncHandler(async (req, res, next) => {
-  const updatedTitle = req.body.title;
-  const updatedCategory = req.body.category;
-  const updatedDescription = req.body.description;
-  const updatedDmage = req.file;
-  const updatedCost = req.body.cost;
+	// const updatedTitle = req.body.title;
+	// const updatedCategory = req.body.category;
+	// const updatedDescription = req.body.description;
+	// const updatedDmage = req.file;
+	// const updatedCost = req.body.cost;
 
-  const productId = req.param.productId;
-  const product = await Product.findById(productId);
+	// const productId = req.param.productId;
+	// const product = await Product.findById(productId);
+	const product = await Product.findByIdAndUpdate(
+		req.params.productId,
+		req.body,
+		{
+			runValidators: true,
+			new: true,
+		},
+	);
+
+	if (!product) {
+		return next(
+			new errorResponse(
+				`Resource not found of id ${req.params.productId}`,
+				404,
+			),
+		);
+	}
+
+	res.status(200).render("product", {
+		product,
+	});
 });
 
 exports.deleteProduct = asyncHandler(async (req, res, next) => {
-  const productId = req.param.productId;
+	const productId = req.param.productId;
 
-  // before deleting we first need to delete the image from the system then we'll delete the product from database
-  const product = await Product.findById(productId);
-  const imagePath = product.imagePath;
+	// before deleting we first need to delete the image from the system then we'll delete the product from database
+	const product = await Product.findById(productId);
+	const imagePath = product.imagePath;
 
-  deleteFile(imagePath);
+	deleteFile(imagePath);
 
-  await Product.deleteOne({ _id: productId });
+	await Product.deleteOne({ _id: productId });
 });
--- server.js
+++ server.js
@@ -18,28 +18,29 @@ const PORT = process.env.PORT || 7000;
 connectDB();
 
 app.set("view engine", "ejs");
+
 // Body parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res, next) => {
-  res.render("home");
+	res.render("home");
 });
 
 // mount routes
 app.use("/users", authRoutes);
-app.use(productRoutes);
+app.use("/products", productRoutes);
 
 // Using Middleware
 app.use(errorHandler);
 
 const server = app.listen(PORT, () => {
-  console.log(`Server started at port ${PORT}`);
+	console.log(`Server started at port ${PORT}`);
 });
 
 // Handle Unhadled Promise rejections
 process.on("unhandledRejection", (err, Promise) => {
-  console.log(`Error: ${err}`);
-  // Close server and exit with 1
-  server.close(() => process.exit(1));
+	console.log(`Error: ${err}`);
+	// Close server and exit with 1
+	server.close(() => process.exit(1));
 });
