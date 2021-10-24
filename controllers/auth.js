afbfa00890f556ce5017fa0f868d13aced7f5ff9
imrcht <rachchooit@gamil.com>
2021-10-24 13:22:45 +0530

    added admin routes and private to admin controller methods

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,14 +5,23 @@ const errorResponse = require("../middleware/error");
 const asyncHandler = require("../middleware/async");
 const secret = require("../security");
 
+// @desc 	Get login page
+// @route 	GET users/login
+// @access	public
 exports.getLogin = (req, res, next) => {
 	res.render("auth/login");
 };
 
+// @desc 	Get register page
+// @route 	GET users/register
+// @access	public
 exports.getRegister = (req, res, next) => {
 	res.render("auth/register");
 };
 
+// @desc 	login a user
+// @route 	POST users/login
+// @access	Public
 exports.postLogin = asyncHandler(async (req, res, next) => {
 	const email = req.body.email;
 	const password = req.body.password;
@@ -34,6 +43,9 @@ exports.postLogin = asyncHandler(async (req, res, next) => {
 	sendTokenResponse(user, 200, res);
 });
 
+// @desc 	Register a user
+// @route 	POST users/register
+// @access	Public
 exports.postRegister = asyncHandler(async (req, res, next) => {
 	const name = req.body.name;
 	const phone = req.body.phone;
@@ -56,10 +68,12 @@ exports.postRegister = asyncHandler(async (req, res, next) => {
 
 	const result = await newUser.save();
 
-	sendTokenResponse(newUser, 201, res);
+	res.status(201).redirect("/users/login");
 });
 
-// Private to admin
+// @desc 	Get single user
+// @route 	GET users/:id
+// @access	Private to Admin
 exports.getUser = asyncHandler(async (req, res, next) => {
 	const user = await User.findById(req.params.id);
 
@@ -72,6 +86,95 @@ exports.getUser = asyncHandler(async (req, res, next) => {
 	res.status(200).json({
 		success: true,
 		user,
+	});
+});
+
+// @desc 	Logout User
+// @route 	GET users/logout
+// @access	Private
+exports.logout = asyncHandler(async (req, res, next) => {
+	res.cookie("token", "none", {
+		expires: new Date(Date.now() + 10 * 1000),
+	});
+
+	res.status(200).json({
+		success: true,
+		message: "User logged out",
+	});
+});
+
+// @desc 	Create User
+// @route 	POST users/user
+// @access	Private to Admin
+exports.createUser = asyncHandler(async (req, res, next) => {
+	const { name, email, phone, password, role } = req.body;
+
+	// Create User in database
+	const user = await User.create({
+		name,
+		email,
+		phone,
+		password,
+		role,
+	});
+
+	res.status(201).json({
+		success: true,
+		message: `${user.role} of name ${user.name} created successfully`,
+	});
+});
+
+// @desc 	User details
+// @route 	GET users/me
+// @access	Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+	const user = await User.findById(req.user.id);
+
+	res.status(200).json({
+		success: true,
+		user,
+	});
+});
+
+// @desc 	Get all User
+// @route 	GET users/allusers
+// @access	Private to Admin
+exports.getUsers = asyncHandler(async (req, res, next) => {
+	res.status(200).json(res.advanceResult);
+});
+
+// @desc 	Update user details
+// @route 	PUT users/:id
+// @access	Private to Admin
+exports.update = asyncHandler(async (req, res, next) => {
+	let user = await User.findById(req.params.id);
+
+	if (!user) {
+		return next(
+			new errorResponse(`User with id ${req.params.id} not found`, 404),
+		);
+	}
+
+	if (req.user.role !== "admin") {
+		if (req.user.id !== req.params.id) {
+			return next(
+				new errorResponse(
+					`${req.user.name} is not allowed to update another user`,
+					401,
+				),
+			);
+		}
+	}
+
+	// Update User
+	user = await User.findByIdAndUpdate(req.params.id, req.body, {
+		runValidators: true,
+		new: true,
+	});
+
+	res.status(200).json({
+		success: true,
+		data: user,
 	});
 });
 
@@ -88,8 +191,5 @@ const sendTokenResponse = (user, statusCode, res) => {
 		httpOnly: true,
 	};
 
-	res.status(statusCode).cookie("token", token, options).json({
-		success: true,
-		user,
-	});
+	res.status(statusCode).cookie("token", token, options).redirect("/");
 };
--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,22 @@ const router = express.Router();
 const { protect, authorize } = require("../middleware/auth");
 const authController = require("../controllers/auth");
 
+// Public routes
 router.get("/login", authController.getLogin);
-
+router.get("/register", authController.getRegister);
+router.post("/register", authController.postRegister);
 router.post("/login", authController.postLogin);
 
-router.get("/register", authController.getRegister);
+// Protected routes
+router.get("/logout", protect, authController.logout);
+router.get("/me", protect, authController.getMe);
 
-router.post("/register", authController.postRegister);
-
-router.route("/:id").get(protect, authorize("admin"), authController.getUser);
+// Admin routes
+router.get("/allusers", protect, authorize("admin"), authController.getUsers);
+router
+	.route("/:id")
+	.put(protect, authorize("admin"), authController.update)
+	.get(protect, authorize("admin"), authController.getUser);
+router.post("/user", protect, authorize("admin"), authController.createUser);
 
 module.exports = router;
