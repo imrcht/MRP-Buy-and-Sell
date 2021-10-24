c7477b78089c7b91a12c3e86e6c9eea26f2303fa
imrcht <rachchooit@gamil.com>
2021-10-24 14:39:39 +0530

    separated admin controller and routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -71,24 +71,6 @@ exports.postRegister = asyncHandler(async (req, res, next) => {
 	res.status(201).redirect("/users/login");
 });
 
-// @desc 	Get single user
-// @route 	GET users/:id
-// @access	Private to Admin
-exports.getUser = asyncHandler(async (req, res, next) => {
-	const user = await User.findById(req.params.id);
-
-	if (!user) {
-		return next(
-			new errorResponse(`User with ${req.params.id} not found`, 404),
-		);
-	}
-
-	res.status(200).json({
-		success: true,
-		user,
-	});
-});
-
 // @desc 	Logout User
 // @route 	GET users/logout
 // @access	Private
@@ -103,27 +85,6 @@ exports.logout = asyncHandler(async (req, res, next) => {
 	});
 });
 
-// @desc 	Create User
-// @route 	POST users/user
-// @access	Private to Admin
-exports.createUser = asyncHandler(async (req, res, next) => {
-	const { name, email, phone, password, role } = req.body;
-
-	// Create User in database
-	const user = await User.create({
-		name,
-		email,
-		phone,
-		password,
-		role,
-	});
-
-	res.status(201).json({
-		success: true,
-		message: `${user.role} of name ${user.name} created successfully`,
-	});
-});
-
 // @desc 	User details
 // @route 	GET users/me
 // @access	Private
@@ -133,48 +94,6 @@ exports.getMe = asyncHandler(async (req, res, next) => {
 	res.status(200).json({
 		success: true,
 		user,
-	});
-});
-
-// @desc 	Get all User
-// @route 	GET users/allusers
-// @access	Private to Admin
-exports.getUsers = asyncHandler(async (req, res, next) => {
-	res.status(200).json(res.advanceResult);
-});
-
-// @desc 	Update user details
-// @route 	PUT users/:id
-// @access	Private to Admin
-exports.update = asyncHandler(async (req, res, next) => {
-	let user = await User.findById(req.params.id);
-
-	if (!user) {
-		return next(
-			new errorResponse(`User with id ${req.params.id} not found`, 404),
-		);
-	}
-
-	if (req.user.role !== "admin") {
-		if (req.user.id !== req.params.id) {
-			return next(
-				new errorResponse(
-					`${req.user.name} is not allowed to update another user`,
-					401,
-				),
-			);
-		}
-	}
-
-	// Update User
-	user = await User.findByIdAndUpdate(req.params.id, req.body, {
-		runValidators: true,
-		new: true,
-	});
-
-	res.status(200).json({
-		success: true,
-		data: user,
 	});
 });
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { protect, authorize } = require("../middleware/auth");
+const { protect } = require("../middleware/auth");
 const authController = require("../controllers/auth");
 
 // Public routes
@@ -12,13 +12,5 @@ router.post("/login", authController.postLogin);
 // Protected routes
 router.get("/logout", protect, authController.logout);
 router.get("/me", protect, authController.getMe);
-
-// Admin routes
-router.get("/allusers", protect, authorize("admin"), authController.getUsers);
-router
-	.route("/:id")
-	.put(protect, authorize("admin"), authController.update)
-	.get(protect, authorize("admin"), authController.getUser);
-router.post("/user", protect, authorize("admin"), authController.createUser);
 
 module.exports = router;
