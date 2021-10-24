7fa8270aad1384ceb09fb4a1b034d976e408057a
imrcht <rachchooit@gamil.com>
2021-10-24 19:53:06 +0530

    changed product routes

--- routes/product.js
+++ routes/product.js
@@ -6,11 +6,15 @@ const { protect, authorize } = require("../middleware/auth");
 // public routes
 router.get("/allproducts", productController.getAllProducts);
 router.get("/productsByCategory", productController.getProductsByCategory);
-router.get("/:productId", productController.getProductById);
 
 // private routes
 router.get("/listproduct", protect, productController.getProductForm);
 router.post("/listproduct", protect, productController.postProduct);
+
+// Public routes
+router.get("/:productId", productController.getProductById);
+
+// Private routes
 router
 	.route("/:productId")
 	.put(protect, productController.updateProduct)
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const path = require("path");
-
 const express = require("express");
 const multer = require("multer");
 const dotenv = require("dotenv");
@@ -26,26 +25,28 @@ connectDB();
 
 app.set("view engine", "ejs");
 
+// Storing file
 const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
+	destination: (req, file, cb) => {
+		cb(null, "images");
+	},
+	filename: (req, file, cb) => {
+		cb(null, file.originalname);
+	},
 });
 
+// filtering file
 const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === "image/jpeg" ||
-    file.mimetype === "image/png" ||
-    file.mimetype === "image/jpg" ||
-    file.mimetype === "image/jfif"
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
+	if (
+		file.mimetype === "image/jpeg" ||
+		file.mimetype === "image/png" ||
+		file.mimetype === "image/jpg" ||
+		file.mimetype === "image/jfif"
+	) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
 };
 
 // Body parser and cookiew parser
@@ -56,12 +57,12 @@ app.use(express.static(__dirname + "/public"));
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+	multer({ storage: fileStorage, fileFilter: fileFilter }).single("image"),
 );
 
 // TESTING ROUTE
 app.get("/", (req, res, next) => {
-  res.render("product/productForm");
+	res.render("home");
 });
 
 // mount routes
@@ -73,12 +74,12 @@ app.use("/admincontrol", adminRoutes);
 app.use(errorHandler);
 
 const server = app.listen(PORT, () => {
-  console.log(`Server started at port ${PORT}`);
+	console.log(`Server started at port ${PORT}`);
 });
 
 // Handle Unhadled Promise rejections
 process.on("unhandledRejection", (err, Promise) => {
-  console.log(`Error: ${err}`);
-  // Close server and exit with 1
-  server.close(() => process.exit(1));
+	console.log(`Error: ${err}`);
+	// Close server and exit with 1
+	server.close(() => process.exit(1));
 });
