f49687ab91ac4cc7bc492bc144b6049718cadc00
imrcht <rachchooit@gamil.com>
2021-10-24 13:55:54 +0530

    dynamic db

--- config/db.js
+++ config/db.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI = `mongodb+srv://MRP:mrpsaleproject@cluster0.7ohbj.mongodb.net/MrpSaleProject`;
+const secret = require("../security");
+
+const MONGO_URI = `mongodb+srv://${secret.username}:${secret.password}@cluster0.7ohbj.mongodb.net/${secret.databaseName}`;
 const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(MONGO_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 		});
-		console.log(`Database Connected`);
+		console.log(`MongoDb Connected: ${conn.connection.host}`);
 	} catch (err) {
 		console.log(err);
 	}
