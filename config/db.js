e301c52720d58d939cbfd19decd7f7a42825c2d9
imrcht <rachchooit@gamil.com>
2021-10-24 13:53:20 +0530

    db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const secret = require("../security");
 
-const MONGO_URI = `mongodb+srv://MRP:mrpsaleproject@cluster0.7ohbj.mongodb.net/MrpSaleProject`;
+const MONGO_URI = `mongodb+srv://${secret.username}:${secret.password}@cluster0.7ohbj.mongodb.net/${secret.databaseName}`;
 const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(MONGO_URI, {
