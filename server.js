2a81880d315c6753914284e0edf311b718483912
imrcht <rachchooit@gamil.com>
2021-11-01 12:22:08 +0530

    added homecontroler separate and also solved req.user problem

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const multer = require("multer");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const cookieParser = require("cookie-parser");
+// home controller
+const homecontroller = require("./controllers/home");
 // auth routes
 const authRoutes = require("./routes/auth");
 // product routes
@@ -27,26 +29,26 @@ app.set("view engine", "ejs");
 
 // Storing file
 const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
+	destination: (req, file, cb) => {
+		cb(null, "images");
+	},
+	filename: (req, file, cb) => {
+		cb(null, file.originalname);
+	},
 });
 
 // filtering file
 const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === "image/jpeg" ||
-    file.mimetype === "image/png" ||
-    file.mimetype === "image/jpg" ||
-    file.mimetype === "image/jfif"
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
+	if (
+		file.mimetype === "image/jpeg" ||
+		file.mimetype === "image/png" ||
+		file.mimetype === "image/jpg" ||
+		file.mimetype === "image/jfif"
+	) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
 };
 
 // Body parser and cookiew parser
@@ -57,35 +59,33 @@ app.use(express.static(__dirname + "/public"));
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+	multer({ storage: fileStorage, fileFilter: fileFilter }).single("image"),
 );
 
 // TESTING ROUTE
-app.get("/", (req, res, next) => {
-  res.render("home");
-});
+app.get("/", homecontroller.getAuth);
 
 // mount routes
 app.use("/users", authRoutes);
 app.use("/products", productRoutes);
 app.use("/admincontrol", adminRoutes);
 app.use("*", (req, res, next) => {
-  res.status(404).render("error", {
-    msg: "Page not Found",
-    statuscode: 404,
-  });
+	res.status(404).render("error", {
+		msg: "Page not Found",
+		statuscode: 404,
+	});
 });
 
 // Using Middleware
 app.use(errorHandler);
 
 const server = app.listen(PORT, () => {
-  console.log(`Server started at port ${PORT}`);
+	console.log(`Server started at port ${PORT}`);
 });
 
 // Handle Unhadled Promise rejections
 process.on("unhandledRejection", (err, Promise) => {
-  console.log(`Error: ${err}`);
-  // Close server and exit with 1
-  server.close(() => process.exit(1));
+	console.log(`Error: ${err}`);
+	// Close server and exit with 1
+	server.close(() => process.exit(1));
 });
