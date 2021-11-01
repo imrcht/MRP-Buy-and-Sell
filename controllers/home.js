44f117d524975cf5ffc273229f7f42300b341ca2
imrcht <rachchooit@gamil.com>
2021-11-01 17:17:03 +0530

    some changes in home controller and logout contorller

--- controllers/home.js
+++ controllers/home.js
@@ -4,21 +4,26 @@ const User = require("../models/User");
 const secret = require("../security");
 
 exports.getAuth = asyncHandler(async (req, res, next) => {
-  let token;
-  token = req.cookies.token;
+	let token;
+	token = req.cookies.token;
 
-  // Verify token
-  if (token) {
-    const decoded = jwt.verify(token, secret.jwt_secret_key);
+	if (token === "none" || !token) {
+		return res.render("home", {
+			name: undefined,
+		});
+	}
 
-    req.user = await User.findById(decoded.id);
+	// Verify token
+	const decoded = jwt.verify(token, secret.jwt_secret_key);
 
-    res.render("home", {
-      name: req.user.name,
-    });
-  } else {
-    res.render("home", {
-      name: undefined,
-    });
-  }
+	req.user = await User.findById(decoded.id);
+
+	if (req.user) {
+		return res.render("home", {
+			name: req.user.name,
+		});
+	}
+	res.render("home", {
+		name: undefined,
+	});
 });
