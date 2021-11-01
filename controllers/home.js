7b000241fa12d5d7d9e8681c4f77872505614902
syedmuneebali99 <alimuneeb3565@gmail.com>
2021-11-01 12:56:07 +0530

    changes in home.ejs home.js

--- controllers/home.js
+++ controllers/home.js
@@ -4,24 +4,21 @@ const User = require("../models/User");
 const secret = require("../security");
 
 exports.getAuth = asyncHandler(async (req, res, next) => {
-	let token;
-	token = req.cookies.token;
+  let token;
+  token = req.cookies.token;
 
-	// Make sure token is present
-	if (!token) {
-		return next(
-			new errorResponse(
-				"You are not authorized to access this route",
-				401,
-			),
-		);
-	}
-	// Verify token
-	const decoded = jwt.verify(token, secret.jwt_secret_key);
+  // Verify token
+  if (token) {
+    const decoded = jwt.verify(token, secret.jwt_secret_key);
 
-	console.log(decoded);
+    req.user = await User.findById(decoded.id);
 
-	req.user = await User.findById(decoded.id);
-	console.log(req.user);
-	res.render("home");
+    res.render("home", {
+      name: req.user.name,
+    });
+  } else {
+    res.render("home", {
+      name: undefined,
+    });
+  }
 });
