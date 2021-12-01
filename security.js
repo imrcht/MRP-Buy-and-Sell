2d371d1a03017a9027e13f31f7b7850debf705b6
imrcht <rachchooit@gamil.com>
2021-12-01 18:21:00 +0530

    deploying website

---
+++ security.js
@@ -0,0 +1,18 @@
+const confi = {
+	provider: "mapquest",
+	apiKey: "lcKxItRh9AYClJiuuBXGxDbzYvEWHbYB",
+	username: "MRP",
+	password: "mrpsaleproject",
+	databaseName: "MrpSaleProject",
+	jwt_secret_key: "jabbajabbajabba",
+	jwt_expire: "1d",
+	jwt_cookie_expire: 1,
+	from_email: "cifehah814@julsard.com",
+	from_name: "Admin-Rachit",
+	sendgrid:
+		"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",
+	fast2sms:
+		"vpEMR6W5fnxHsh9Y3rdeAPXGomOilykTDULJq2V7tQ4zNabcKgvbjihy1C2gcUBlEndmrP6fV4YSawpZ",
+};
+
+module.exports = confi;
