86d8c846d4484cb04838e907bc03abef72763801
Rachit gupta <rachchooit@gmail.com>
2024-07-04 23:02:15 +0530

    change db uri

--- config/db.js
+++ config/db.js
@@ -2,17 +2,19 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 // const secret = require("../security");
 
-const MONGO_URI = `mongodb+srv://${process.env.user}:${process.env.password}@cluster0.7ohbj.mongodb.net/MrpSaleProject`;
+const MONGO_URI = `mongodb+srv://${process.env.user}:${process.env.password}@cluster0.vhg7m.mongodb.net/${process.env.databaseName}`;
 const connectDB = async () => {
-	try {
-		const conn = await mongoose.connect(MONGO_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
-		console.log(`MongoDb Connected: ${conn.connection.host}`);
-	} catch (err) {
-		console.log(err);
-	}
+  try {
+    console.log(MONGO_URI);
+    const conn = await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDb Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = connectDB;
+
